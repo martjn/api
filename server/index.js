7f9a25c33f45a27d72794084f1e3da0bada02212
martjn <mingdev77@gmail.com>
2023-11-06 22:37:34 +0200

    hide personal content to env file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
@@ -8,14 +8,14 @@ app.use(express.json());
 const db = require("./models");
 
 // Routers
-const postRouter = require('./routes/Posts')
-app.use("/posts", postRouter)
-const commentsRouter = require('./routes/Comments')
-app.use("/comments", commentsRouter)
-const usersRouter = require('./routes/Users')
-app.use("/auth", usersRouter)
-const likesRouter = require('./routes/Likes')
-app.use("/likes", likesRouter)
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
+const usersRouter = require("./routes/Users");
+app.use("/auth", usersRouter);
+const likesRouter = require("./routes/Likes");
+app.use("/likes", likesRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
