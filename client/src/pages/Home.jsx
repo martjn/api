fd46fdfa92812f889ab36097696d46fb204b6830
martjn <mingdev77@gmail.com>
2023-11-19 19:09:45 +0200

    Add side navigation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,12 +6,17 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { AuthContext } from "../helpers/AuthContext";
 import CommentIcon from "@mui/icons-material/Comment";
+import TimeAgo from "../Components/TimeAgo/TimeAgo";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import { HeartIcon } from "@heroicons/react/24/solid";
 
 function Home() {
   const [listOfPosts, setListOfPosts] = useState([]);
   const [likedPosts, setLikedPosts] = useState([]);
   const [commentData, setCommentData] = useState([]);
   const { authState } = useContext(AuthContext);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(window.innerWidth >= 1280);
+  const [sortBy, setSortBy] = useState("date");
   let navigate = useNavigate();
 
   useEffect(() => {
@@ -23,9 +28,13 @@ function Home() {
           headers: {
             accessToken: localStorage.getItem("accessToken"),
           },
+          params: {
+            sortBy: sortBy,
+          },
         })
         .then((response) => {
           setListOfPosts(response.data.listOfPosts);
+          console.log("set listofposts")
           setLikedPosts(
             response.data.likedPosts.map((value) => {
               return value.PostId;
@@ -51,7 +60,7 @@ function Home() {
           }
         });
     }
-  }, []);
+  }, [sortBy]);
 
   const onLike = (postId) => {
     axios
@@ -95,62 +104,108 @@ function Home() {
       });
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSidebarOpen(window.innerWidth >= 1280);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleSort = (value) => {
+    setSortBy(value);
+    console.log("sort by", value)
+  }
+
   return (
-    <div className="flex flex-col items-center">
-      {listOfPosts.map((value, key) => {
-        return (
-          <div
-            key={key}
-            className="p-4 w-1/2 border-b border-gray-300 rounded-lg bg-gray-900"
-          >
-            <div className="text-sm text-gray-400">{`@${value.username} · ${value.createdAt}`}</div>
-            <div className="text-lg my-2 font-bold text-white rounded">
-              {value.title}
-            </div>
-            <div className="text-gray-400 mb-2 text-sm whitespace-pre-line">
-              {value.postText}
-            </div>
-            <div class="flex flex-row">
-              <div class="flex flex-row text-white hover:text-orange-900 transition-all mr-3 bg-blue-gray-900 rounded-lg p-1">
-                {likedPosts.includes(value.id) ? (
-                  <FavoriteIcon
+    <div class="flex flex-col">
+      <div class="flex flex-col items-center my-4">
+        <span class="text-white font-bold text-2xl">
+          Welcome to PostIt, {authState.username}
+        </span>
+      </div>
+      <div className="flex my-2 items-center justify-end max-w-screen-2xl">
+        <span className="text-white">Sort by</span>
+        <button
+          onClick={() => handleSort("date")}
+          className={`${
+            sortBy === "date" ? "bg-blue-gray-500 text-white" : "bg-gray-500"
+          } mx-2 rounded-md`}
+        >
+          <AccessTimeIcon/>
+        </button>
+        <button
+          onClick={() => handleSort("popularity")}
+          className={`${
+            sortBy === "popularity" ? "bg-blue-gray-500 text-white" : "bg-gray-500"
+          } mx-2 rounded-md`}
+        >
+          <FavoriteIcon/>
+        </button>
+      </div>
+      <div className="flex flex-col items-center w-fit border-x border-t rounded-sm border-blue-gray-100 border-opacity-50 md:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto">
+        {listOfPosts.map((value, key) => {
+          return (
+            <div
+              key={key}
+              className="p-4 w-full bg-black border-b border-opacity-50 border-blue-gray-100"
+            >
+              <div className="text-sm text-gray-400">
+                {`@${value.username} · `}
+                <TimeAgo date={value.createdAt} />
+              </div>
+              <div className="text-lg my-2 font-bold text-white rounded">
+                {value.title}
+              </div>
+              <div className="text-gray-400 mb-2 text-sm whitespace-pre-line">
+                {value.postText}
+              </div>
+              <div class="flex flex-row">
+                <div class="flex flex-row text-white hover:text-orange-900 transition-all mr-3 bg-blue-gray-900 rounded-lg p-1">
+                  {likedPosts.includes(value.id) ? (
+                    <FavoriteIcon
+                      className="cursor-pointer"
+                      onClick={() => {
+                        onLike(value.id);
+                      }}
+                    />
+                  ) : (
+                    <FavoriteBorderIcon
+                      className="cursor-pointer"
+                      onClick={() => {
+                        onLike(value.id);
+                      }}
+                    />
+                  )}
+
+                  <label className="ml-1 text-md font-bold rounded-lg">
+                    {value.Likes.length}
+                  </label>
+                </div>
+
+                <div class="flex flex-row text-white hover:text-orange-900 transition-all bg-blue-gray-900 rounded-lg p-1">
+                  <CommentIcon
                     className="cursor-pointer"
                     onClick={() => {
-                      onLike(value.id);
+                      navigate(`/post/${value.id}`);
                     }}
                   />
-                ) : (
-                  <FavoriteBorderIcon
-                    className="cursor-pointer"
-                    onClick={() => {
-                      onLike(value.id);
-                    }}
-                  />
-                )}
-
-                <label className="ml-1 text-md font-bold rounded-lg">
-                  {value.Likes.length}
-                </label>
-              </div>
-
-              <div class="flex flex-row text-white hover:text-orange-900 transition-all bg-blue-gray-900 rounded-lg p-1">
-                <CommentIcon
-                  className="cursor-pointer"
-                  onClick={() => {
-                    navigate(`/post/${value.id}`);
-                  }}
-                />
-                <label className="ml-1 text-md font-bold rounded-lg">
-                  {
-                    commentData.find((comment) => comment.postId === value.id)
-                      ?.commentAmount
-                  }
-                </label>
+                  <label className="ml-1 text-md font-bold rounded-lg">
+                    {
+                      commentData.find((comment) => comment.postId === value.id)
+                        ?.commentAmount
+                    }
+                  </label>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
